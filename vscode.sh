cbe8d927f4d1874e86289cef37f230e6a40f7a50
Do young An <doyoung@toysmyth.com>
2020-05-26 13:55:44 +0900

    Express 라우트 스니펫 추가

--- vscode.sh
+++ vscode.sh
@@ -13,3 +13,4 @@ echo "----------------------------"
 cp ./vscode/typescript.json ~/Library/Application\Support/Code/User/snippets
 cp ./vscode/typescriptreact.json ~/Library/Application\Support/Code/User/snippets
 
+echo "Success Copy!!!"
